43707ce56fec7cc74cd6cbf69cafd71f7dd9b57e
javiergonzalezh <j.h.gonzalez@sheffield.ac.uk>
2014-11-11 10:42:50 +0000

    wrap classed merged into bayesian_optimization

--- GPyOpt/methods/__init__.py
+++ GPyOpt/methods/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2014, GPyOpt authors (see AUTHORS.txt).
 # Licensed under the BSD 3-clause license (see LICENSE.txt)
 
-from .bayesian_optimizationEI import BayesianOptimizationEI
-from .bayesian_optimizationMPI import BayesianOptimizationMPI
-from .bayesian_optimizationUCB import BayesianOptimizationUCB
+#from .bayesian_optimizationEI import BayesianOptimizationEI
+#from .bayesian_optimizationMPI import BayesianOptimizationMPI
+#from .bayesian_optimizationLCB import BayesianOptimizationLCB
+from .bayesian_optimization import BayesianOptimization
