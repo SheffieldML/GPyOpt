32ed916686901bc95db3d3b2dd504b989f7a8a41
javiergonzalezh <j.h.gonzalez@sheffield.ac.uk>
2014-11-19 23:59:20 +0000

    function for physical experiment added

---
+++ GPyOpt/fmodels/physical.py
@@ -0,0 +1,23 @@
+class PhysicalExperiment: 
+
+    def __init__(self):
+    
+    def PhysicalExperiment(X):
+        n, p = X.shape
+    
+        if n==1:
+            print 'Experimental conditions:'
+            print X
+            feval = input('Insert here experimental value:')
+            output =np.array([[feval]])
+    
+        else:
+            output = np.zeros(n).T    
+            print 'Batch of %s samples is requiered:' %n
+            print '---------------------------------'
+            for k in range(n):
+            print 'Experimental conditions for sample %s:' %k+1
+            print X[k,]
+            output[k] = input('Insert experimental value:')
+    
+        return output
