c4859a810e1ec85e98883249942fb0733b8e23b1
Zhenwen Dai <dai@fias.uni-frankfurt.de>
2015-02-02 23:59:13 +0100

    cut matplotlib

--- GPyOpt/fmodels/experimentsNd.py
+++ GPyOpt/fmodels/experimentsNd.py
@@ -1,7 +1,10 @@
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
-import matplotlib.pyplot as plt
+try:
+        from mpl_toolkits.mplot3d import Axes3D
+        from matplotlib import cm
+        from matplotlib.ticker import LinearLocator, FormatStrFormatter
+        import matplotlib.pyplot as plt
+except:
+        pass
 import numpy as np
 from scipy.optimize import minimize
 from ..util.general import reshape
