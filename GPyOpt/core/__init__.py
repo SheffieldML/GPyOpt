bee2c99f5d56fc6a831b26a7f0220f94dd8e13b8
javiergonzalezh <j.h.gonzalez@sheffield.ac.uk>
2015-03-14 02:06:48 +0000

    folder for web created

--- GPyOpt/core/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2015, GPyOpt authors (see AUTHORS.txt).
-# Licensed under the BSD 3-clause license (see LICENSE.txt)
-
-import acquisition
-import bo
-import optimization
--- GPyOpt/plotting/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2012, GPyOpt authors (see AUTHORS.txt).
-# Licensed under the BSD 3-clause license (see LICENSE.txt)
-
-try:
-    import plots_bo
-except:
-    pass
