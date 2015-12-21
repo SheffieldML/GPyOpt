0981b48f1898a562a18d283f2152ee7b03b432ac
System Administrator <j.h.gonzalez@sheffield.ac.uk>
2015-12-21 22:09:29 +0000

    added first unit test

--- GPyOpt/testing/bo_tests.py
+++ GPyOpt/testing/bo_tests.py
@@ -14,17 +14,11 @@ class TestEI(unittest.TestCase):
         self.bo = GPyOpt.methods.BayesianOptimization(f=self.bjective.f,   # function to optimize       
                                                     bounds=bounds,          # box-constrains of the problem
                                                     acquisition_type='EI')  
-
+        self.bo.run_optimization(max_iter=1, eps=10e-6) 
         
     def test_bo(self):
         # Run bo optimization
-        self.bo.run_optimization(max_iter=1, eps=10e-6) 
-      	print self.bo.suggested_sample[0][0]
-        self.assertEqual(0,0)
-
-        # Check the successfulness of test
         self.assertEqual(self.bo.suggested_sample[0][0],0.66656362406227299)
-
 
 
 if __name__ == '__main__':
