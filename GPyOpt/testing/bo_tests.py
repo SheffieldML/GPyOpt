8ceb8e27547599e02ef4b4342d4214786f24b384
System Administrator <j.h.gonzalez@sheffield.ac.uk>
2015-12-21 22:03:04 +0000

    added first unit test

--- GPyOpt/testing/bo_tests.py
+++ GPyOpt/testing/bo_tests.py
@@ -1,16 +1,32 @@
 import unittest
 import numpy as np
+import GPyOpt
+from numpy.random import seed
 
-class BOTest(unittest.TestCase):
+class TestEI(unittest.TestCase):
     
     def setUp(self):
         #initialize model
-        self.bo = None
+        seed(1234)
+        self.bjective  = GPyOpt.fmodels.experiments1d.forrester()
+        bounds = [(0,1)]  
+
+        self.bo = GPyOpt.methods.BayesianOptimization(f=self.bjective.f,   # function to optimize       
+                                                    bounds=bounds,          # box-constrains of the problem
+                                                    acquisition_type='EI')  
+
         
     def test_bo(self):
         # Run bo optimization
-        # bo.optimize
+        self.bo.run_optimization(max_iter=1, eps=10e-6) 
+      	print self.bo.suggested_sample[0][0]
+        self.assertEqual(0,0)
 
         # Check the successfulness of test
-        self.assertTrue(True)
+        self.assertEqual(self.bo.suggested_sample[0][0],0.66656362406227299)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
         
