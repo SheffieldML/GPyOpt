02e5b71ce7950f2f8f532ecff5647b148e9c518d
Javier Gonzalez <j.h.gonzalez@sheffield.ac.uk>
2016-02-04 18:06:42 +0000

    plot fixed and illustrative demo for modular optmization added

--- GPyOpt/demos/__init__.py
+++ GPyOpt/demos/__init__.py
@@ -6,5 +6,7 @@ from .begginer_optimization_1d import begginer_optimization_1d, simple_optimizat
 from .advanced_optimization_2d import advanced_optimization_2d
 from .parallel_optimization import parallel_optimization
 from .big_data_optimization import big_data_optimization
+from .modular_optimization import modular_optimization
+
 from .autoML_tuner import autoML_tuner
 
--- GPyOpt/demos/modular_optimization.py
+++ GPyOpt/demos/modular_optimization.py
@@ -1,26 +1,26 @@
-def simple_optimization_1d(plots=True):
+def modular_optimization(plots=True):
     import GPyOpt
     from numpy.random import seed
     seed(1234)
         
     # --- Fucntion to optimize
     func  = GPyOpt.objective_examples.experiments1d.forrester()  
-    cost = lambda x: 2*x
+    #cost = lambda x: 2*x
 
     # --- Space design
     space = GPyOpt.Design_space([{'domain':(0,1)}])
 
     # --- Object of 
-    objective = GPyOpt.core.task.SingleObjective(func.f, space, cost)
+    objective = GPyOpt.core.task.SingleObjective(func.f, space)
 
     # --- CHOOSE the model type
-    model = GPyOpt.models.GPModel(exact_feval=True, num_hmc_samples = 100)
+    model = GPyOpt.models.GPModel(exact_feval=True)
     
     # --- CHOOSE the acquisition optimizer
-    aquisition_optimizer = GPyOpt.optimization.ContAcqOptimizer(space, 50)
+    aquisition_optimizer = GPyOpt.optimization.ContAcqOptimizer(space, 100)
     
     # --- CHOOSE the type of acquisition
-    acquisition = GPyOpt.acquisitions.AcquisitionLCB_hmc(model, space, optimizer=aquisition_optimizer)
+    acquisition = GPyOpt.acquisitions.AcquisitionEI(model, space, optimizer=aquisition_optimizer)
     
     # --- CHOOSE the type of acquisition
     initial_design = GPyOpt.util.stats.initial_design('random', space.get_continuous_bounds(), 5)
@@ -29,18 +29,20 @@ def simple_optimization_1d(plots=True):
     bo = GPyOpt.core.BO(model, space, objective, acquisition, initial_design)
 
     # --- Problem definition and optimization
-    max_time = 20                                                         
+    max_time  = 20 
+    max_iter  = 10
+    tolerance = 1e-8                                                         
 
     print '-----'
     print '----- Running demo. It may take a few seconds.'
     print '-----'
     
     # Run the optimization                                                  
-    bo.run_optimization(max_time=max_time, eps=1e-8) 
+    bo.run_optimization(max_iter = max_iter, max_time = max_time, eps = tolerance) 
                             
 
     # --- Plots
     if plots:
-        objective_true.plot()
-        #bo.plot_acquisition()
-        #bo.plot_convergence()
+        func.plot()
+        bo.plot_acquisition()
+        bo.plot_convergence()
