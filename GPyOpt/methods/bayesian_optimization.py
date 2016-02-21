0bd8975b89a689cd6569e66eb633a6ab2d06bfef
Javier Gonzalez <j.h.gonzalez@sheffield.ac.uk>
2016-02-21 22:43:32 +0000

    added wrapper class

--- GPyOpt/methods/bayesian_optimization.py
+++ GPyOpt/methods/bayesian_optimization.py
@@ -1,32 +1,151 @@
-# # Copyright (c) 2015, Javier Gonzalez
-# # Copyright (c) 2015, the GPy Authors (see GPy AUTHORS.txt)
-# # Licensed under the BSD 3-clause license (see LICENSE.txt)
+# Copyright (c) 2015, Javier Gonzalez
+# Copyright (c) 2015, the GPy Authors (see GPy AUTHORS.txt)
+# Licensed under the BSD 3-clause license (see LICENSE.txt)
 
-# import GPy
-# import deepgp
-# import numpy as np
-# import time
-# from ..core.acquisition import AcquisitionEI, AcquisitionMPI, AcquisitionLCB, AcquisitionEL
-# from ..core.bo import BO
-# from ..util.general import samples_multidimensional_uniform, reshape, evaluate_function
-# from ..util.stats import initial_design
-# import warnings
-# warnings.filterwarnings("ignore")
-
-
-# class BayesianOptimization(BO):
-
-# # new interface
-#     def __init__(self, f, bounds=None, model_type=None, X=None, Y=None, initial_design_numdata = None, 
-#         initial_design_type='random', model_optimize_interval=1, acquisition_type ='EI', acquisition_par = 0.00, 
-#         model_optimize_restarts=3, normalize=False, exact_feval=False, verbosity=0):
+import GPy
+import deepgp
+import numpy as np
+import time
+from ..core.acquisition import AcquisitionEI, AcquisitionMPI, AcquisitionLCB, AcquisitionEL
+from ..core.bo import BO
+from ..util.general import samples_multidimensional_uniform, reshape, evaluate_function
+from ..util.stats import initial_design
+import warnings
+warnings.filterwarnings("ignore")
 
 
-# # old interface
-#     def __init__(self, f, bounds=None, kernel=None, X=None, Y=None, numdata_initial_design = None, 
-#         type_initial_design='random', model_optimize_interval=1, acquisition='EI',
-#         acquisition_par= 0.00, model_optimize_restarts=10, sparseGP=False, num_inducing=None, normalize=False,
-#         exact_feval=False, verbosity=0):
+class BayesianOptimization(BO):
+
+    def __init__(self, f, domain = None, constrains = None, cost = None, model_type=None, X=None, Y=None, 
+    	initial_design_numdata = None, initial_design_type='random', model_optimize_interval=1, acquisition_type ='EI', 
+    	acquisition_par = 0.00, model_optimize_restarts=3, normalize=False, exact_feval=False, verbosity=0):
+
+        # --- minimal options for defining the problem
+        self.f      = f
+        self.cost   = cost
+        self.domain = domain
+        constrains  = constrains
+        self.cost   = cost
+
+        # --- minimal options to define the model 
+        self.model_type  = model_type  # None
+        self.exact_feval = exact_feval # False 
+        self.normalize_Y = normalize_Y # True  
+
+        # --- minimal options to define the acquisition
+        acquisition_type ='EI'
+
+        # --- Initial values for Xs and Ys
+        X = None 
+        Y = None 
+        initial_design_numdata = None, 
+        initial_design_type='random', 
+
+
+        
+        acquisition_type ='EI', 
+        acquisition_par = 0.00, 
+        model_optimize_restarts=3, 
+        normalize=False, 
+        exact_feval=False, 
+        verbosity=0
+
+
+        # --- Create design space
+        self.space = GPyOpt.Design_space(space=self.domain, constrains=self.constrains)
+
+        # --- Create objective function
+        self.objective = GPyOpt.core.task.SingleObjective(self.f, self.space, self.cost)
+
+        # --- CHOOSE the model type
+        self.model = model_chooser()
+
+        # --- CHOOSE the acquisition optimizer
+        self.aquisition_optimizer = GPyOpt.optimization.ContAcqOptimizer(space, 1000, search=True)
+
+        # --- CHOOSE the type of acquisition
+        self.acquisition = self.acquisition_chooser()
+
+        # --- CHOOSE the intial design
+        initial_design = initial_design_chooser()
+
+        # -- Define an optimization space
+        super(BayesianOptimization ,self).__init__(	model = self.model, 
+                									space = self.space, 
+                									objective = self.objective, 
+                									acquisition_func = self.acquisition, 
+                									X_init = initial_design, 
+                									normalize_Y = normalize_Y, 
+                									model_update_interval = model_update_interval)
+
+
+	def model_chooser(self):
+
+        # Extra arguments to define the model, all in kargs
+        kernel=None, 
+        noise_var=None, 
+        optimizer='bfgs', 
+        max_iters      = 1000, 
+        optimize_restarts=1
+        num_inducing     = 10
+        model_optimize_interval=1 
+
+
+		if self.model_type == 'gp':
+
+            return 
+
+        elif self.model_type == 'sparse_gp':
+            return 
+
+        elif self.model_type == 'deep_gp':
+            return 
+
+        elif self.model_type == 'rf':
+
+
+
+    def acquisition_chooser(self):
+
+        # --- Extract relevant parameters from the ***kargs
+        if self.kargs.has_key('acquisition_jitter'):
+            self.acquisition_jitter = kargs['acquisition_jitter']
+        else:
+            self.acquisition_jitter = 0.01
+
+        if self.kargs.has_key('acquisition_weight'):
+            self.acquisition_weight = kargs['acquisition_weight']
+        else:
+            self.acquisition_weight = 2  ## TODO: implement the optimal rate (only for bandits)
+
+
+        # --- Choose the acquisition
+        elif self.acquisition_type == None or acquisition_type =='EI':
+            return AcquisitionEI(self.model, self.space, self.optimizer, self.cost, self.acquisition_jitter)
+        
+        elif self.acquisition_type =='EI_MCMC':
+            return AcquisitionEI_MCMC(self.model, self.space, self.optimizer, self.cost, self.acquisition_jitter)        
+        
+        elif self.acquisition_type =='MPI':
+            return AcquisitionMPI(self.model, self.space, self.optimizer, self.cost, self.acquisition_jitter)
+         
+        elif self.acquisition_type =='MPI_MCMC':
+            return AcquisitionMPI_MCMC(self.model, self.space, self.optimizer, self.cost, self.acquisition_jitter)
+
+        elif self.acquisition_type =='LCB':
+            return AcquisitionLCB(self.model, self.space, self.optimizer, cself.cost, self.acquisition_weight)
+        
+        elif self.acquisition_type =='LCB_MCMC':
+            return AcquisitionLCB_MCMC(self.model, self.space, self.optimizer, self.cost, self.acquisition_weight) 
+        
+        else:
+            raise Exception('Invalid acquisition selected.')
+
+
+
+
+
+
 
 
 
