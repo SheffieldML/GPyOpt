3565e7bded9c6b8ed54aa4d53ac59789c29a1259
Andreas <adamianou@gmail.com>
2016-04-08 17:57:40 +0100

    Fixes for deepGPyOpt

--- GPyOpt/demos/__init__.py
+++ GPyOpt/demos/__init__.py
@@ -3,10 +3,10 @@
 # Licensed under the BSD 3-clause license (see LICENSE.txt)
 
 from .begginer_optimization_1d import begginer_optimization_1d
-from .advanced_optimization_2d import advanced_optimization_2d
-from .parallel_optimization import parallel_optimization
-from .big_data_optimization import big_data_optimization
-from .modular_optimization import modular_optimization
+# from .advanced_optimization_2d import advanced_optimization_2d
+# from .parallel_optimization import parallel_optimization
+# from .big_data_optimization import big_data_optimization
+# from .modular_optimization import modular_optimization
 
-from .autoML_tuner import autoML_tuner
+# from .autoML_tuner import autoML_tuner
 
--- GPyOpt/models/deepgpmodel.py
+++ GPyOpt/models/deepgpmodel.py
@@ -6,13 +6,15 @@ from .base import BOModel
 import numpy as np
 import GPy
 import deepgp
+from GPy.core.parameterization.variational import VariationalPosterior, NormalPosterior
+
 
 class DeepGPModel(BOModel):
 
     analytical_gradient_prediction = False
     
     def __init__(self, kernel=None, noise_var=None, exact_feval=False, normalize_Y=True, optimizer='bfgs', max_iters=1000, 
-    			optimize_restarts=5, num_inducing = 10, back_constraint=True, repeatX=True, verbose=False):          
+    			optimize_restarts=5, num_inducing = 10, back_constraint=True, repeatX=True, verbose=False, max_init_iters=100):          
 
         self.noise_var = noise_var
         self.exact_feval = exact_feval
@@ -20,11 +22,12 @@ class DeepGPModel(BOModel):
         self.optimize_restarts = optimize_restarts
         self.optimizer = optimizer
         self.max_iters = max_iters
+        self.max_init_iters = max_init_iters
         self.verbose = verbose
         self.back_constraint =back_constraint
         self.repeatX =repeatX
-        self.num_inducing = num_inducing
-        self.kernel = kernel
+        self.model_num_inducing = num_inducing
+        self.model_kernel = kernel
         self.Ds = 1
 
 
@@ -38,47 +41,63 @@ class DeepGPModel(BOModel):
         self.X = X
         self.Y = Y
 
-        import socket
         self.useGPU = False
-        if socket.gethostname()[0:4] == 'node':
-            print 'Using GPU!'
-            self.useGPU = True
 
         # --- kernel and dimension of the hidden layer
-        if self.kernel == None:
-        	self.kernel = [GPy.kern.Matern32(self.Ds, ARD=False), GPy.kern.Matern32(self.X.shape[1], ARD=False)]
+        if self.model_kernel == None:
+        	# self.kernel = [GPy.kern.Matern32(self.Ds, ARD=False), GPy.kern.Matern32(self.X.shape[1], ARD=False)]
+            self.kernel = [GPy.kern.RBF(self.Ds, ARD=True), GPy.kern.RBF(self.X.shape[1], ARD=True)]
         else:
-        	self.kernel = self.kernel  # this need to be one kernel per layer
+        	self.kernel = [k.copy() for k in self.model_kernel]  # this need to be one kernel per layer
 
         # Type of deepGPs
         if self.back_constraint:
-            self.model = deepgp.DeepGP([self.Y.shape[1],self.Ds, self.X.shape[1]], Y=self.Y, X=self.X, num_inducing=self.num_inducing, kernels=self.kernel, MLP_dims=[[100,50],[]])#, repeatX=self.repeatX)
-        
+            self.model = deepgp.DeepGP([self.Y.shape[1],self.Ds, self.X.shape[1]], Y=self.Y, X=self.X, num_inducing=self.num_inducing, kernels=self.kernel, MLP_dims=[[100,50],[]], repeatX=self.repeatX)
         else:
-            self.model = deepgp.DeepGP([self.Y.shape[1],self.Ds, self.X.shape[1]], Y=self.Y, X=self.X, num_inducing=self.num_inducing, kernels=self.kernel, back_constraint=False) #, repeatX=self.repeatX)
+            self.model = deepgp.DeepGP([self.Y.shape[1],self.Ds, self.X.shape[1]], Y=self.Y, X=self.X, num_inducing=self.num_inducing, kernels=self.kernel, back_constraint=False, repeatX=self.repeatX)
 
         if self.exact_feval == True:
             self.model.obslayer.Gaussian_noise.constrain_fixed(1e-6, warning=False) #to avoid numerical problems
         else:
-            self.model.obslayer.Gaussian_noise.constrain_bounded(1e-6,1e6, warning=False) #to avoid numerical problems
+            self.model.obslayer.Gaussian_noise.constrain_positive(warning=False) #to avoid numerical problems
 
 
     def updateModel(self, X_all, Y_all, X_new, Y_new):
+        import numpy as np
 
         if self.normalize_Y:
         	Y_all = (Y_all - Y_all.mean())/(Y_all.std())
 
-		self._create_model(X_all, Y_all)
+        # Do not use more inducing than data
+        self.num_inducing = np.min((self.model_num_inducing, Y_all.shape[0]))
+
+        self._create_model(X_all, Y_all)
 
 		# TOD Optimization: we can still imrpve this part
-		self.m_init = self.model.copy()
+        for i in range(len(self.model.layers)):
+            if isinstance(self.model.layers[i].Y, NormalPosterior) or isinstance(self.model.layers[i].Y, VariationalPosterior):
+                cur_var = self.model.layers[i].Y.mean.var()
+            else:
+                cur_var = self.model.layers[i].Y.var()
+            self.model.layers[i].Gaussian_noise.variance = cur_var / 100.
+
+            self.model.layers[i].Gaussian_noise.variance.fix(warning=False)
+
+        self.model.optimize(optimizer = self.optimizer, messages=self.verbose, max_iters=self.max_init_iters)
+
+        for i in range(len(self.model.layers)):
+            self.model.layers[i].Gaussian_noise.variance.constrain_positive(warning=False)
+
+
 		# self.model.obslayer['Gaussian_noise.variance'].fix()
 		# self.model.layer_1['Gaussian_noise.variance'].fix()
 		# self.model.optimize(optimizer = self.optimizer, messages=self.verbose, max_iters=self.max_iters)
 		# self.model_init2 = self.modelcopy()
 		# self.model.obslayer['Gaussian_noise.variance'].constrain_positive()
 		# self.model.layer_1['Gaussian_noise.variance'].constrain_positive()
-		self.model.optimize(optimizer = self.optimizer, messages=self.verbose, max_iters=self.max_iters)
+        self.model.optimize(optimizer = self.optimizer, messages=self.verbose, max_iters=self.max_iters)
+        deepgp.util.check_snr(self.model) 
+
 
 
     def predict(self, X):
