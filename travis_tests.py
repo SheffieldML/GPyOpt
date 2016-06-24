4ca7a573417013b4092039d9844c2a6b86b937bb
Zhenwen Dai <z.dai@sheffield.ac.uk>
2016-06-24 13:49:55 +0100

    add travis_tests.py

---
+++ travis_tests.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import matplotlib
+matplotlib.use('agg')
+
+import nose, warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+nose.main('GPyOpt', defaultTest='GPyOpt/testing/', argv=['', '--show-skipped'])
