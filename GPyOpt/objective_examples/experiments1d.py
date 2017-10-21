91ee15b0004e9a8018732f55af398288abf7dd4f
Ryan J. Kinnear <Ryan@Kinnear.ca>
2017-10-21 12:27:54 -0400

    Allow to specify an axis for function plotting.

--- GPyOpt/objective_examples/experiments1d.py
+++ GPyOpt/objective_examples/experiments1d.py
@@ -8,29 +8,43 @@ import numpy as np
 class function1d:
 	'''
 	This is a benchmark of unidimensional functions interesting to optimize. 
-	:param bounds: the box constraints to define the domain in which the function is optimized.
 	'''
-	def plot(self,bounds=None):
-		if bounds is  None: bounds = self.bounds
+	def plot(self, bounds=None, ax=None):
+		'''
+		:param bounds: the box constraints to define the domain in which
+		the function is optimized.
+		:param ax: (optional) The axis to plot to
+		'''
+		if bounds is None:
+			bounds = self.bounds
+		if ax is None:
+			fig = plt.figure()
+			ax = fig.add_subplot(1, 1, 1)
+			call_show = True
+		else:
+			call_show = False
 		X = np.arange(bounds[0][0], bounds[0][1], 0.01)
 		Y = self.f(X)
-		plt.plot(X, Y, lw=2)
-		plt.xlabel('x')
-		plt.ylabel('f(x)')
-		plt.show()
+
+		ax.plot(X, Y, lw=2, label='f')
+		ax.legend()
+		ax.set_xlabel('x')
+		ax.set_ylabel('f(x)')
+		if call_show:
+			plt.show()
+		return
 
 class forrester(function1d):
-	'''
-	Forrester function. 
-	
-	:param sd: standard deviation, to generate noisy evaluations of the function.
+	'''Forrester function.
+	:param sd: standard deviation, to generate
+	  noisy evaluations of the function.
 	'''
 	def __init__(self,sd=None):
 		self.input_dim = 1		
 		if sd==None: self.sd = 0
 		else: self.sd=sd
-		self.min = 0.78 		## approx
-		self.fmin = -6 			## approx
+		self.min = 0.78			## approx
+		self.fmin = -6			## approx
 		self.bounds = [(0,1)]
 
 	def f(self,X):
