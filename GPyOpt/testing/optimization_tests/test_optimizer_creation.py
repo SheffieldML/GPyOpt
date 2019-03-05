3247a2b95f1cf7d806ece5eda232141207812d67
Andrei Paleyes <paleyes@amazon.com>
2019-03-05 15:50:58 +0000

    Fixed obvious errors in CMA and DIRECT optimizers, added unit tests

--- GPyOpt/testing/optimization_tests/test_optimizer_creation.py
+++ GPyOpt/testing/optimization_tests/test_optimizer_creation.py
@@ -1,18 +1,45 @@
+import numpy as np
 import unittest
 
-from GPyOpt.optimization.optimizer import choose_optimizer
 from GPyOpt.core.errors import InvalidVariableNameError
+from GPyOpt.core.task.space import Design_space
+from GPyOpt.optimization.optimizer import choose_optimizer
 
 
 class TestOptimizerCreation(unittest.TestCase):
-    """
-    Test creation of optimizers
-    """
+    def __init__(self, *args, **kwargs):
+        super(TestOptimizerCreation, self).__init__(*args, **kwargs)
+
+        self.space = [
+            {'name': 'var_1', 'type': 'continuous', 'domain': (-1, 1), 'dimensionality': 1},
+            {'name': 'var_2', 'type': 'continuous', 'domain': (-1, 1), 'dimensionality': 1}
+        ]
+        self.design_space = Design_space(self.space)
+        self.f = lambda x: np.sum(np.sin(x))
 
     def test_invalid_optimizer_name_raises_error(self):
-        """
-        Test error thrown for invalid name
-        """
-
         self.assertRaises(InvalidVariableNameError,
                           choose_optimizer, 'asd', None)
+
+    def test_create_lbfgs_optimizer(self):
+        optimizer = choose_optimizer('lbfgs', self.design_space.get_bounds())
+        result = optimizer.optimize([0.0, 0.0], self.f)
+
+        self.assertIsNotNone(result)
+
+    def test_create_direct_optimizer(self):
+        optimizer = choose_optimizer('DIRECT', self.design_space.get_bounds())
+        result = optimizer.optimize([0.0, 0.0], self.f)
+
+        self.assertIsNotNone(result)
+
+    def test_create_cma_optimizer(self):
+        optimizer = choose_optimizer('CMA', self.design_space.get_bounds())
+        result = optimizer.optimize([0.0, 0.0], self.f)
+
+        self.assertIsNotNone(result)
+
+    def test_create_cma_optimizer_invalid_dimentions(self):
+        optimizer = choose_optimizer('CMA', self.design_space.get_bounds()[0:1])
+        with self.assertRaises(IndexError):
+            optimizer.optimize([0.0, 0.0], self.f)
