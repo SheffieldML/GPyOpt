d3dbb0f2724ba823fb216ae74327c85ae8a5516b
floyebolu <zcbeg36@ucl.ac.uk>
2019-11-29 01:02:41 +0000

    Add test case for ei acquisition constraint check
    Adds cases that test that acquisition functions with and without
    gradients do correct constraint check.

--- GPyOpt/testing/acquisitions_tests/test_ei_acquisition.py
+++ GPyOpt/testing/acquisitions_tests/test_ei_acquisition.py
@@ -59,3 +59,47 @@ class TestEIAcquisition(unittest.TestCase):
         optimum_position = self.ei_acquisition.optimize()
 
         assert optimum_position == expected_optimum_position
+
+
+class TestEIAcquisitionWithCategoricalVariables(unittest.TestCase):
+	def setUp(self):
+		self.mock_model = Mock()
+		self.mock_optimizer = Mock()
+		
+		domain = [{'name': 'var_1', 'type': 'categorical', 'domain': (0, 1)}, {'name': 'var_2', 'type': 'continuous', 'domain': (-5,5), 'dimensionality': 2}]
+		# con_1: if var_1 is 0, var_2_1 must be <= -1
+		# con_2: 3 * (var_2_1 + var_2_2) <= 24
+		constraints = [{'name': 'con_1', 'constraint': '(x[:,0] == 0) * (x[:,1] + 1)'}, 
+						{'name': 'con_2', 'constraint': ' 3 * (x[:,1] + x[:,2]) - 24'}]
+		self.space = Design_space(domain, constraints)
+
+		self.ei_acquisition = AcquisitionEI(self.mock_model, self.space, self.mock_optimizer)
+		self.ei_acquisition._compute_acq = Mock()
+		self.ei_acquisition._compute_acq_withGradients = Mock()
+
+	def test_acquisition_function(self):
+		"""Test that acquisition function does correct constraint(s) check"""
+		
+		y = [1.37, 8.22, 4.2, 0.55, 3.14]
+		self.ei_acquisition._compute_acq.return_value = np.array(y)[:, None]
+		correct_y = [-y[0]] + [0,0] + [-y[3]] + [0]
+		x_unzipped = np.array([[0, 1, 3.3, -3.3], [1, 0, 1.5, 4.7], [0, 1, 4.1, 4.5], [1, 0, -4.1, 4.5], [1, 0, 5, 3]])
+		
+		acquisitions = self.ei_acquisition.acquisition_function(x_unzipped)
+		
+		assert np.isclose(acquisitions, np.array(correct_y)[:, None]).all()
+	
+	def test_acquisition_function_withGradients(self):
+		"""Test that acquisition function does correct constraint(s) check with gradients"""
+
+		y = [1.37, 8.22, 4.2, 0.55, 3.14]
+		y_grad = [.3, .7, -.5, .1, -.02]
+		self.ei_acquisition._compute_acq_withGradients.return_value = np.array(y)[:, None], np.array(y_grad)[:, None]
+		correct_y = [-y[0]] + [0,0] + [-y[3]] + [0]
+		correct_y_grad = [-y_grad[0]] + [0,0] + [-y_grad[3]] + [0]
+		x_unzipped = np.array([[0, 1, 3.3, -3.3], [1, 0, 1.5, 4.7], [0, 1, 4.1, 4.5], [1, 0, -4.1, 4.5], [1, 0, 5, 3]])
+
+		acquisitions, gradients = self.ei_acquisition.acquisition_function_withGradients(x_unzipped)
+
+		assert np.isclose(acquisitions, np.array(correct_y)[:, None]).all()
+		assert np.isclose(gradients, np.array(correct_y_grad)[:, None]).all()
